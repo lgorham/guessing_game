531bd1b80fd0e00950ff4409e09e9f02c89fb5fe
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:33:14 -0700

    Greeting the Player

---
+++ game.py
@@ -0,0 +1,20 @@
+# Put your code here
+import random
+
+name = raw_input("What is your name? ")
+random_int = random.randint(1,100)
+guess = int(raw_input("Hello %s Pick an integer between 1 and 100: " % name))
+previous_guesses = []
+print random_int
+
+while guess != random_int:
+    if guess < random_int:
+        print "Guess is too low, try again"
+    elif guess > random_int:
+        print "Guess is too high, try again"
+    previous_guesses.append(guess)
+    guess = int(raw_input("Enter a new guess: "))
+
+print "You guessed it! It took you %s tries" % len(previous_guesses)
+
+
